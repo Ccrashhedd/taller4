8c23113a6de10f0f1e3dcf9fe7c37f8a32cd4bcc
Ccrashh <SMoralesF1403@gmail.com>
2026-04-16 16:43:02 -0500

    subida de cosas

---
+++ app/src/main/java/com/example/taller4/AccionesMat/Acciones.kt
@@ -0,0 +1,23 @@
+package com.example.taller4.AccionesMat
+
+
+
+class Acciones {
+
+    fun sumar(a: Float, b: Float): Float {
+        return a + b
+    }
+
+    fun restar(a: Float, b:Float): Float {
+        return a - b
+    }
+
+    fun multiplicacion (a: Float, b: Float): Float {
+        return a * b
+    }
+
+    fun division (a: Float, b: Float): Float {
+        return a / b
+    }
+
+}
